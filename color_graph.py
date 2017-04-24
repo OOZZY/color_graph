771d21a09428ceea68cfd9944dbe0d8306d242e3
oozzy <oozzy.tlo@gmail.com>
2017-04-24 12:43:28 -0700

    Add minimum remaining values heuristic and degree heuristic

--- color_graph.py
+++ color_graph.py
@@ -27,8 +27,8 @@ def parse_text(text):
         item = text_splits[i]
         item_splits = string_array_to_int_array(item.split())
         vertex = item_splits[0]
-        connected_vertexes = set(item_splits[1:])
-        graph[vertex] = connected_vertexes
+        adjacent_vertexes = set(item_splits[1:])
+        graph[vertex] = adjacent_vertexes
 
     return num_vertexes, num_colors, graph
 
@@ -45,32 +45,126 @@ class Problem(object):
         return set(self.graph.keys())
 
     def assignment_is_complete(self, assignment):
+        assert vertexes(assignment).issubset(self.vertexes())
+
         return self.vertexes() == vertexes(assignment)
 
     def unassigned_vertexes(self, assignment):
+        assert vertexes(assignment).issubset(self.vertexes())
+
         return self.vertexes() - vertexes(assignment)
 
     def colors(self):
-        return range(num_colors)
+        return set(range(num_colors))
 
     def assignment_is_consistent(self, assignment):
+        assert vertexes(assignment).issubset(self.vertexes())
+
         for vertex in vertexes(assignment):
-            connected_vertexes = self.graph[vertex]
-            for connected_vertex in connected_vertexes:
-                if connected_vertex in vertexes(assignment):
-                    if assignment[vertex] == assignment[connected_vertex]:
+            adjacent_vertexes = self.graph[vertex]
+            for adjacent_vertex in adjacent_vertexes:
+                if adjacent_vertex in vertexes(assignment):
+                    if assignment[vertex] == assignment[adjacent_vertex]:
                         return False
 
         return True
+
+    def choose_unassigned_vertex_without_heuristic(self, assignment):
+        assert vertexes(assignment).issubset(self.vertexes())
+
+        unassigned_vertexes = self.unassigned_vertexes(assignment)
+        return next(iter(unassigned_vertexes))
+
+    def remaining_colors(self, assignment, vertex):
+        assert vertexes(assignment).issubset(self.vertexes())
+        unassigned_vertexes = self.unassigned_vertexes(assignment)
+        assert vertex in unassigned_vertexes
+
+        used_colors = set()
+        adjacent_vertexes = self.graph[vertex]
+        for adjacent_vertex in adjacent_vertexes:
+            if adjacent_vertex in vertexes(assignment):
+                used_colors.add(assignment[adjacent_vertex])
+
+        return self.colors() - used_colors
+
+    def num_remaining_colors(self, assignment, vertex):
+        assert vertexes(assignment).issubset(self.vertexes())
+        unassigned_vertexes = self.unassigned_vertexes(assignment)
+        assert vertex in unassigned_vertexes
+
+        return len(self.remaining_colors(assignment, vertex))
+
+    def degree(self, assignment, vertex):
+        assert vertexes(assignment).issubset(self.vertexes())
+        unassigned_vertexes = self.unassigned_vertexes(assignment)
+        assert vertex in unassigned_vertexes
+
+        adjacent_vertexes = self.graph[vertex]
+        unassigned_adjacent_vertexes = adjacent_vertexes - vertexes(assignment)
+        return len(unassigned_adjacent_vertexes)
+
+    def vertexes_with_minimum_remaining_colors(self, assignment, vertex_set):
+        assert vertexes(assignment).issubset(self.vertexes())
+        unassigned_vertexes = self.unassigned_vertexes(assignment)
+        assert vertex_set.issubset(unassigned_vertexes)
+
+        first = next(iter(vertex_set))
+        minimum_remaining_colors = self.num_remaining_colors(assignment, first)
+        vertexes_to_return = set([first])
+
+        for vertex in vertex_set:
+            num_remaining_colors = self.num_remaining_colors(assignment, vertex)
+
+            if num_remaining_colors < minimum_remaining_colors:
+                minimum_remaining_colors = num_remaining_colors
+                vertexes_to_return = set([vertex])
+            elif num_remaining_colors == minimum_remaining_colors:
+                vertexes_to_return.add(vertex)
+
+        return vertexes_to_return
+
+    def vertexes_with_highest_degree(self, assignment, vertex_set):
+        assert vertexes(assignment).issubset(self.vertexes())
+        unassigned_vertexes = self.unassigned_vertexes(assignment)
+        assert vertex_set.issubset(unassigned_vertexes)
+
+        first = next(iter(vertex_set))
+        highest_degree = self.degree(assignment, first)
+        vertexes_to_return = set([first])
+
+        for vertex in vertex_set:
+            degree = self.degree(assignment, vertex)
+
+            if degree > highest_degree:
+                highest_degree = degree
+                vertexes_to_return = set([vertex])
+            elif degree == highest_degree:
+                vertexes_to_return.add(vertex)
+
+        return vertexes_to_return
+
+    def choose_unassigned_vertex_with_heuristic(self, assignment):
+        assert vertexes(assignment).issubset(self.vertexes())
+
+        unassigned_vertexes = self.unassigned_vertexes(assignment)
+        unassigned_vertexes = self.vertexes_with_minimum_remaining_colors(
+            assignment, unassigned_vertexes)
+        unassigned_vertexes = self.vertexes_with_highest_degree(
+            assignment, unassigned_vertexes)
+        return next(iter(unassigned_vertexes))
 
 def recursive_backtracking_search(assignment, problem):
     if problem.assignment_is_complete(assignment):
         return assignment
 
-    unassigned_vertexes = problem.unassigned_vertexes(assignment)
-    vertex = min(unassigned_vertexes)
+    # vertex = problem.choose_unassigned_vertex_without_heuristic(assignment)
+    vertex = problem.choose_unassigned_vertex_with_heuristic(assignment)
 
-    for color in problem.colors():
+    # colors = problem.colors()
+    colors = problem.remaining_colors(assignment, vertex)
+
+    for color in colors:
         assignment[vertex] = color
         if problem.assignment_is_consistent(assignment):
             result = recursive_backtracking_search(assignment, problem)
@@ -80,7 +174,6 @@ def recursive_backtracking_search(assignment, problem):
                 return result
 
     return None
-
 
 def backtracking_search(problem):
     return recursive_backtracking_search({}, problem)
